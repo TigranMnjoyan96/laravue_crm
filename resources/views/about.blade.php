c37a1411a006815ca636e0643d3e2200fd08a0d7
TigranMnjoyan96 <mnjoyan1996@gmail.com>
2020-05-31 23:14:22 +0400

    'routing'

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+
+
+@section('content')
+    <h2>About page</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+
+Route::get('/about', 'AboutController@index')->name('about');
